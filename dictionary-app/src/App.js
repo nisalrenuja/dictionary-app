fd1e3f753da5a04b4f3011eced84b9539981b7e0
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-08-20 04:30:52 +0530

    Header added

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+//Create context
+export const InputContext = createContext();
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
+
+  const value = {
+    inputValue,
+    setInputValue,
+  };
+
+  return (
+    <InputContext.Provider value={value}>
+      <div className="App">
+        <Header />
+      </div>
+    </InputContext.Provider>
+  );
+}
+
+export default App;

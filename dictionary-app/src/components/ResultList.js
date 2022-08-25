9f4c76f142a12e7705b1e2914459ef8cfcb5f78a
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-08-26 04:08:15 +0530

    definitions added

---
+++ dictionary-app/src/components/ResultList.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useContext, useState, useEffect } from "react";
+import { InputContext } from "../App";
+
+axios.defaults.baseURL = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
+const ResultList = () => {
+  const { inputValue } = useContext(InputContext);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (param) => {
+    try {
+      setLoading(true);
+      const res = await axios(`/${param}`);
+      setResponse(res.data);
+      setError(null);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (inputValue.length) {
+      fetchData(inputValue);
+    }
+  }, [inputValue]);
+
+  if (loading) {
+    return <h1 className="text-center">Loading...</h1>;
+  }
+
+  if (error) {
+    return (
+      <h3 className="text-center mt-10 font-semibold text-gray-500">
+        No Definition Found
+      </h3>
+    );
+  }
+
+  return (
+    <div className="container mx-auto p-4 max-w-2xl">
+      {response && (
+        <div>
+          <h3 className="text-2xl font-bold mt-4">Meaning & Definition:</h3>
+          <h3 className="text-2xl font-bold mt-4">Example:</h3>
+          <h3 className="text-2xl font-bold mt-4">Synonym:</h3>
+          <h3 className="text-2xl font-bold mt-4">Antonym:</h3>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ResultList;

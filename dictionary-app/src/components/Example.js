928e386bf2ce6276f543880bce8dbe92272108ca
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-08-26 23:32:52 +0530

    fixed

---
+++ dictionary-app/src/components/Example.js
@@ -0,0 +1,17 @@
+const Example = ({ mean }) => {
+  return (
+    <div>
+      {mean.map((val) =>
+        val.meanings.map((means) =>
+          means.definitions.map((def) => (
+            <div key={def.example}>
+              {def.example ? <li>{def.example}</li> : ""}
+            </div>
+          ))
+        )
+      )}
+    </div>
+  );
+};
+
+export default Example;
--- dictionary-app/src/components/MeaningList.js
+++ dictionary-app/src/components/MeaningList.js
@@ -1,16 +1,12 @@
 const MeaningList = ({ mean }) => {
   return (
     <div>
-      {mean.map((val) =>
-        val.meanings.map((means) =>
-          means.definitions.map((def) => (
-            <div key={def.definition}>
-              <li>{def.definition}</li>
-              <hr />
-            </div>
-          ))
-        )
-      )}
+      {mean.map(val => val.meanings.map(means => means.definitions.map(def => (
+        <div key={def.definition}>
+          <li>{def.definition}</li>
+          <hr />
+        </div>
+      ))))}
     </div>
   );
 };

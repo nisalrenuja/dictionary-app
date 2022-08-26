391593826fba8fa11fb0d84acbfc558eff0649b5
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-08-26 18:19:51 +0530

    meaning list file added

---
+++ dictionary-app/src/components/MeaningList.js
@@ -0,0 +1,18 @@
+const MeaningList = ({ mean }) => {
+  return (
+    <div>
+      {mean.map((val) =>
+        val.meanings.map((means) =>
+          means.definitions.map((def) => (
+            <div key={def.definition}>
+              <li>{def.definition}</li>
+              <hr />
+            </div>
+          ))
+        )
+      )}
+    </div>
+  );
+};
+
+export default MeaningList;
